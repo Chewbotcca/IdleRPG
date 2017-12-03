e9f37bb36707776d7676f636113a3c477ab49f67
Chewsterchew <chew@chew.pw>
2017-12-03 14:53:37 -0600

    Add digest gem.

--- requiregems.rb
+++ requiregems.rb
@@ -15,3 +15,16 @@ require 'json'
 require 'net/http'
 require 'yaml'
 require 'open-uri'
+begin
+  require 'digest'
+rescue LoadError
+  puts "You're missing the gem `digest`. Would you like to install this now? (y/n)"
+  input = gets.chomp
+  if input == 'y'
+    `gem install digest`
+    puts 'Gem installed! Continuing..'
+  else
+    puts 'To continue, install the digest gem'
+    exit
+  end
+end
