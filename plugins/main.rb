b60bef7368d92b65741335759e2d51efd5e1ca1f
Chewsterchew <chew@chew.pw>
2017-12-02 20:17:35 -0600

    hey, look! It's version 0.0.1

---
+++ plugins/main.rb
@@ -0,0 +1,28 @@
+class Main
+  include Cinch::Plugin
+
+  match /setup/, method: :setup
+
+  def setup(m)
+    m.reply 'Setting up the channel for IdleRPG'
+    begin
+      m.channel.mode "+v #{CONFIG['nickname']}"
+    rescue
+      m.reply 'I do not have permission voice users! Aborting!'
+      return
+    end
+    channel = m.channel.to_s[1..m.channel.to_s.length]
+    filename = "data/channels/#{channel}.yaml"
+    unless File.exist?(filename)
+      File.new(filename, 'w+')
+      exconfig = YAML.load_file('data/channels/channel.example.yaml')
+      File.open(filename, 'w') { |f| f.write exconfig.to_yaml }
+    end
+    data = YAML.load_file(filename)
+    data['name'] = channel
+    data['ownerhost'] = m.user.host
+    data['users'] = 0
+    File.open(filename, 'w') { |f| f.write data.to_yaml }
+    m.reply "Channel ready! Run `/msg #{CONFIG['nickname']} register [username] [password] [classname]`"
+  end
+end
